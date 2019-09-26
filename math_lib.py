7705da32171aa7a0b6d7d76223088ad4e49d0987
mchifala <mich5456@colorado.edu>
2019-09-26 02:42:40 +0000

    Add test for calculating mean of random list

--- math_lib.py
+++ math_lib.py
@@ -1,6 +1,12 @@
+import sys
 
 def list_mean(L):
-    return None
+    try:
+        return sum(L)/len(L)
+
+    except ZeroDivisionError as inst:
+        print("Run-Time Error:", type(inst))
+        sys.exit(1)
 
 def list_stdev(L):
     return None
--- test_math.py
+++ test_math.py
@@ -4,12 +4,20 @@ from math_lib import list_stdev
 import random
 import os
 import numpy as np
+import sys
 
 class TestMath(unittest.TestCase):
     
     def test_mean_empty(self):
         self.assertRaises(ZeroDivisionError and
                           SystemExit, list_mean, [])
+    
+    def test_mean(self):
+        for i in range(100):
+            self.L = np.random.randint(0, 100, size=100)
+            self.assertEqual(list_mean(self.L), np.mean(self.L))
+        
+    
     
 if __name__ == '__main__':
     unittest.main()
