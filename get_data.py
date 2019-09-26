66aef151054bbdc75e7b0651c2bbf2b6e3ff905c
mchifala <mich5456@colorado.edu>
2019-09-26 14:54:04 +0000

    Add viz.py module

--- get_data.py
+++ get_data.py
@@ -1,3 +1,13 @@
+import sys
 
 def read_stdin_col(col_num):
-    return None
+    L = []
+    for line in sys.stdin:
+        line = line.strip().split()
+        L.append(int(line[col_num]))
+
+    return L
+        
+
+if __name__ == '__main__':
+    read_stdin_col(0)
