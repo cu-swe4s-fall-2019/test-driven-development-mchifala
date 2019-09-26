9278e17b4115fdb0f06b90e3a5e532c10f379da4
mchifala <mich5456@colorado.edu>
2019-09-26 03:50:14 +0000

    Add test for calculating mean of invalid input types (None) in list

--- test_math.py
+++ test_math.py
@@ -38,14 +38,18 @@ class TestMath(unittest.TestCase):
             self.L = np.random.randint(0, 100, size=50)
             self.L = np.append(self.L, np.random.rand(50))
             self.assertAlmostEqual(list_stdev(self.L), np.std(self.L))
-    
+
     def test_mean_string(self):
         self.assertRaises(TypeError and
-                          SystemExit, list_mean, [3,"A"])
-        
+                          SystemExit, list_mean, [3, "A"])
+
     def test_std_string(self):
         self.assertRaises(TypeError and
-                          SystemExit, list_stdev, [3,"A"])
+                          SystemExit, list_stdev, [3, "A"])
+
+    def test_mean_string(self):
+        self.assertRaises(TypeError and
+                          SystemExit, list_mean, [3, None])
 
 
 if __name__ == '__main__':
