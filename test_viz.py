42e54507a91b1730f0cb5faa3e62c014ecf0e8e4
mchifala <mich5456@colorado.edu>
2019-09-26 18:25:40 +0000

    Change .travis.yml file and add visualization unit tests

---
+++ test_viz.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from data_viz import boxplot
+from data_viz import histogram
+from data_viz import combo
+import unittest
+import matplotlib
+matplotlib.use('Agg')
+
+
+class TestPlots(unittest.TestCase):
+
+    def test_histogram_exists(self):
+        self.L = np.random.randint(0, 100, size=100)
+        self.file = "histogram_test"
+        histogram(self.L, self.file)
+        self.assertEqual(True, os.path.exists(self.file+".png"))
+
+    def test_histogram_not_exist(self):
+        self.L = np.random.randint(0, 100, size=100)
+        self.file = "histogram_test2"
+        histogram(self.L, "bad_histogram")
+        self.assertEqual(False, os.path.exists(self.file+".png"))
+
+    def test_boxplot_exist(self):
+        self.L = np.random.randint(0, 100, size=100)
+        self.file = "boxplot_test"
+        boxplot(self.L, self.file)
+        self.assertEqual(True, os.path.exists(self.file+".png"))
+
+    def test_boxplot_not_exist(self):
+        self.L = np.random.randint(0, 100, size=100)
+        self.file = "boxplot_test2"
+        boxplot(self.L, "bad_boxplot")
+        self.assertEqual(False, os.path.exists(self.file+".png"))
+
+    def test_combo_exist(self):
+        self.L = np.random.randint(0, 100, size=100)
+        self.file = "combo_test"
+        combo(self.L, self.file)
+        self.assertEqual(True, os.path.exists(self.file+".png"))
+
+    def test_combo_not_exist(self):
+        self.L = np.random.randint(0, 100, size=100)
+        self.file = "combo_test2"
+        combo(self.L, "bad_combo")
+        self.assertEqual(False, os.path.exists(self.file+".png"))
+
+
+if __name__ == '__main__':
+    unittest.main()
