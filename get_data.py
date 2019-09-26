bc98c448c5c165fe924a1df7fa9633d848c4b575
mchifala <mich5456@colorado.edu>
2019-09-26 17:35:21 +0000

    Add error handling for non-existent column number

--- get_data.py
+++ get_data.py
@@ -4,7 +4,13 @@ def read_stdin_col(col_num):
     L = []
     for line in sys.stdin:
         line = line.strip().split()
-        L.append(int(line[col_num]))
+        
+        try:
+            L.append(int(line[col_num]))
+            
+        except IndexError as inst:
+            print("Run-Time Error:", type(inst))
+            sys.exit(1)
 
     return L
         
