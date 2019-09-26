bf82edcce59fbfc990594795d0e36ec7a71d1ecd
mchifala <mich5456@colorado.edu>
2019-09-26 02:48:57 +0000

    Add test for calculating standard deviation of random list

--- test_math.py
+++ test_math.py
@@ -7,20 +7,25 @@ import numpy as np
 import sys
 
 class TestMath(unittest.TestCase):
-    
+
     def test_mean_empty(self):
         self.assertRaises(ZeroDivisionError and
                           SystemExit, list_mean, [])
-    
-    def test_mean(self):
+
+    def test_mean_random(self):
         for i in range(100):
             self.L = np.random.randint(0, 100, size=100)
             self.assertEqual(list_mean(self.L), np.mean(self.L))
-            
+
     def test_std_empty(self):
-        self.assertRaises(SystemExit, list_stdev, [])
-        
-    
-    
+        self.assertRaises(ZeroDivisionError and
+                          SystemExit, list_stdev, [])
+
+    def test_std_random(self):
+        for i in range(100):
+            self.L = np.random.randint(0, 100, size=100)
+            self.assertEqual(list_std(self.L), np.std(self.L))
+
+
 if __name__ == '__main__':
     unittest.main()
